04df4fcdad3bdb57fbcfaeed142fb79876166143
Stukova Olya <stukova.o@gmail.com>
2024-11-30 14:53:48 +0500

    feat(api): migrate data input methods to use Float32Array

--- src/stories/cosmos.ts
+++ src/stories/cosmos.ts
@@ -2,12 +2,18 @@ import { GraphConfigInterface } from '@/graph/config'
 import { Graph } from '@/graph/index'
 
 export type CosmosStoryProps = GraphConfigInterface & {
-  pointPositions: number[];
-  links: number[];
-  pointColors: number[];
+  pointPositions: Float32Array;
+  pointColors: Float32Array;
+  pointSizes?: Float32Array;
+
+  links?: Float32Array;
+  linkColors?: Float32Array;
+  linkWidths?: Float32Array;
+  // linkStrength?: Float32Array;
+
   pointClusters?: number[];
   clusterPositions?: number[];
-  clusterForces?: number[];
+  clusterForces?: Float32Array;
 }
 
 export const createCosmos = (props: CosmosStoryProps): HTMLCanvasElement => {
@@ -45,8 +51,12 @@ export const createCosmos = (props: CosmosStoryProps): HTMLCanvasElement => {
 
     graph.setPointPositions(props.pointPositions)
     graph.setPointColors(props.pointColors)
+    if (props.pointSizes) graph.setPointSizes(props.pointSizes)
 
     if (props.links) graph.setLinks(props.links)
+    if (props.linkColors) graph.setLinkColors(props.linkColors)
+    if (props.linkWidths) graph.setLinkWidths(props.linkWidths)
+    // if (props.linkStrength) graph.setLinkStrength(props.linkStrength)
 
     if (props.pointClusters) graph.setPointClusters(props.pointClusters)
     if (props.clusterPositions) graph.setClusterPositions(props.clusterPositions)
