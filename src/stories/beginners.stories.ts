bbce12fdb61129f6bd4ddb8b3b633975cb2e5090
Stukova Olya <stukova.o@gmail.com>
2025-02-26 12:47:02 +0500

    Storybook: add interactive add/remove points story for beginners

--- src/stories/beginners.stories.ts
+++ src/stories/beginners.stories.ts
@@ -5,6 +5,7 @@ import { CosmosStoryProps } from './create-cosmos'
 import { QuickStartStory } from './beginners/quick-start'
 import { BasicSetUpStory } from './beginners/basic-set-up'
 import { PointLabelsStory } from './beginners/point-labels'
+import { AddRemovePoints } from './beginners/add-remove-points'
 
 import quickStartStoryRaw from './beginners/quick-start?raw'
 import basicSetUpStoryRaw from './beginners/basic-set-up/index?raw'
@@ -14,6 +15,10 @@ import pointLabelsStoryRaw from './beginners/point-labels/index?raw'
 import pointLabelsStoryDataRaw from './beginners/point-labels/data.ts?raw'
 import pointLabelsStoryLabelsRaw from './beginners/point-labels/labels.ts?raw'
 import pointLabelsStoryCssRaw from './beginners/point-labels/style.css?raw'
+import addRemovePointsStoryRaw from './beginners/add-remove-points/index?raw'
+import addRemovePointsStoryCssRaw from './beginners/add-remove-points/style.css?raw'
+import addRemovePointsStoryConfigRaw from './beginners/add-remove-points/config.ts?raw'
+import addRemovePointsStoryDataGenRaw from './beginners/add-remove-points/data-gen.ts?raw'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<CosmosStoryProps> = {
@@ -71,5 +76,17 @@ export const PointLabels: Story = {
   },
 }
 
+export const AddRemovePointsStory: Story = {
+  ...createStory(AddRemovePoints),
+  name: 'Add / Remove Points',
+  parameters: {
+    sourceCode: [
+      { name: 'Story', code: addRemovePointsStoryRaw },
+      { name: 'config.ts', code: addRemovePointsStoryConfigRaw },
+      { name: 'data-gen.ts', code: addRemovePointsStoryDataGenRaw },
+      { name: 'style.css', code: addRemovePointsStoryCssRaw },
+    ],
+  },
+}
 // eslint-disable-next-line import/no-default-export
 export default meta
---
+++ src/stories/beginners/add-remove-points/config.ts
@@ -0,0 +1,23 @@
+import { GraphConfigInterface } from '@cosmograph/cosmos'
+
+export const config: GraphConfigInterface = {
+  spaceSize: 4096,
+  backgroundColor: '#151515',
+  pointSize: 4,
+  pointColor: '#4B5BBF',
+  pointGreyoutOpacity: 0.1,
+  linkWidth: 0.1,
+  linkColor: '#5F74C2',
+  linkArrows: false,
+  linkGreyoutOpacity: 0,
+  hoveredPointCursor: 'pointer',
+  renderHoveredPointRing: true,
+  fitViewDuration: 1000,
+  fitViewPadding: 0.3,
+  enableSimulationDuringZoom: true,
+  simulationLinkDistance: 1,
+  simulationLinkSpring: 2,
+  simulationRepulsion: 0.2,
+  simulationGravity: 0.1,
+  simulationDecay: 100000,
+}
